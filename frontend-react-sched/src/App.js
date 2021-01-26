d1f40ec5c3104be9556d731f9aafab3014e9ceb4
garcikg0 <kg.gar91@gmail.com>
2021-01-26 14:54:10 -0500

    Login component created no styling

--- frontend-react-sched/src/App.js
+++ frontend-react-sched/src/App.js
@@ -1,9 +1,19 @@
+import React, { useState } from 'react';
+import { Route, Switch, Redirect, withRouter, useHistory } from 'react-router-dom';
 import './index.css';
 import WeeklyCal from './components/weeklyCal/WeeklyCal';
+import LoginForm from './components/loginForm/LoginForm';
 
-function App() {
+const App = () => {
+
+  const [currentUser, setCurrentUser] = useState(null);
+
   return (
-    <div class="container">
+    <>
+      <LoginForm />
+    <Switch>
+      <Route path='/home' exact>
+      <div class="container">
       <div class="side-bar"> SIDE BAR </div>
       <div class="weekly-cal-container">
         <WeeklyCal />
@@ -15,7 +25,10 @@ function App() {
       <div class="work-to-do">WORK TO DO</div>
       <div class="post-it">POST IT</div>
     </div>
+      </Route>
+    </Switch>
+    </>
   );
 }
 
-export default App;
+export default withRouter(App);
---
+++ frontend-react-sched/src/components/loginForm/LoginForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './styles.scss';
+
+const LoginForm = () => {
+
+    return (
+        <div>Test</div>
+    )
+}
+
+export default LoginForm;
