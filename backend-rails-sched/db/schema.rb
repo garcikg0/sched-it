cf6cad51aee29b7d541ea86096f121e89909c89f
garcikg0 <kg.gar91@gmail.com>
2021-01-26 14:13:23 -0500

    db & migrations created

---
+++ backend-rails-sched/db/schema.rb
@@ -0,0 +1,81 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_01_26_191016) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "calendars", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_calendars_on_user_id"
+  end
+
+  create_table "entries", force: :cascade do |t|
+    t.bigint "journal_id", null: false
+    t.date "created_date"
+    t.string "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["journal_id"], name: "index_entries_on_journal_id"
+  end
+
+  create_table "journals", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_journals_on_user_id"
+  end
+
+  create_table "notes", force: :cascade do |t|
+    t.bigint "calendar_id", null: false
+    t.datetime "start_datetime"
+    t.datetime "end_datetime"
+    t.string "title"
+    t.string "description"
+    t.string "location"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["calendar_id"], name: "index_notes_on_calendar_id"
+  end
+
+  create_table "todos", force: :cascade do |t|
+    t.bigint "calendar_id", null: false
+    t.date "start_date"
+    t.date "due_date"
+    t.date "completed_date"
+    t.string "title"
+    t.string "description"
+    t.string "category"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["calendar_id"], name: "index_todos_on_calendar_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "username"
+    t.string "password_digest"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "calendars", "users"
+  add_foreign_key "entries", "journals"
+  add_foreign_key "journals", "users"
+  add_foreign_key "notes", "calendars"
+  add_foreign_key "todos", "calendars"
+end
